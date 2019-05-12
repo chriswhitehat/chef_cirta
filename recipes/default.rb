36b225cae545acff166c710d8e73cf08a08b5f5b
Chris White <chriswhitehat@gmail.com>
2019-05-12 10:39:12 -0700

    init

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,31 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+group 'ir' do
+  action :create
+  gid 999
+end
+
+
+dirs = ['/nsm',
+        '/nsm/scripts',
+        '/nsm/scripts/python',
+        '/nsm/scripts/python/cirta']
+
+dirs.each |dir| do
+  directory dir do
+    owner 'root'
+    group 'ir'
+    mode '0770'
+    action :create
+  end
+end
+
+git '/nsm/scripts/python/cirta' do
+  repository 'https://github.com:chriswhitehat/cirta.git'
+  reference 'master'
+  user 'root'
+  group 'ir'
+  action :sync
+end

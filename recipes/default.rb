0c9f491c9ec70d3757ddc4e478f39f30741b034e
Chris White <chriswhitehat@gmail.com>
2019-05-12 23:48:50 -0700

    a bunch of dirs

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,10 +19,14 @@ pip_packages.each do |pip_name, pip_dir_name|
   end
 end
 
+##########################
+# Base directories
+##########################
+
 dirs = ['/nsm',
         '/nsm/scripts',
-        '/nsm/scripts/python',
-        '/nsm/scripts/python/cirta']
+        '/nsm/scripts/python'
+      ]
 
 dirs.each do |dir|
   directory dir do
@@ -33,6 +37,11 @@ dirs.each do |dir|
   end
 end
 
+
+##########################
+# Base directories
+##########################
+
 git '/nsm/scripts/python/cirta' do
   repository 'https://github.com/chriswhitehat/cirta.git'
   reference 'master'
@@ -40,3 +49,58 @@ git '/nsm/scripts/python/cirta' do
   group 'ir'
   action :sync
 end
+
+
+
+##########################
+# Local directories
+##########################
+
+dirs = ['/nsm',
+        '/nsm/scripts',
+        '/nsm/scripts/python',
+        '/nsm/scripts/python/cirta',
+        '/nsm/scripts/python/cirta/etc/local',
+        '/nsm/scripts/python/cirta/plugins/local',
+        '/nsm/scripts/python/cirta/plugins/local/actions',
+        '/nsm/scripts/python/cirta/plugins/local/initializers',
+        '/nsm/scripts/python/cirta/plugins/local/sources'
+      ]
+
+dirs.each do |dir|
+  directory dir do
+    owner 'root'
+    group 'ir'
+    mode '0770'
+    action :create
+  end
+end
+
+
+##########################
+# Plugin __init__.py
+##########################
+
+['', '/actions', '/initializers', '/sources'].each do |plugin_type|
+  file "/nsm/scripts/python/cirta/plugins/local#{plugin_type}/__init__.py" do
+    action :create
+    owner 'root'
+    group 'ir'
+    mode '0640'
+  end
+end
+
+
+##########################
+# User directories
+##########################
+
+node[:users].each do |username|
+  directory "/nsm/scripts/python/cirta/etc/#{username}" do
+    owner username
+    group username
+    mode '0700'
+    action :create
+  end
+end
+

bfc2c92587f38ba9ca7d58a29108c47d7e1051f0
Chris White <chriswhitehat@gmail.com>
2019-08-05 03:10:51 -0700

    added whois package dep

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
-package ['git', 'python2.7', 'python-ldap', 'python-pip', 'python-ipcalc', 'nbtscan']
+package ['git', 'python2.7', 'python-ldap', 'python-pip', 'python-ipcalc', 'nbtscan', 'whois']
 
 
 pip_packages = [['pytz', 'pytz'],
